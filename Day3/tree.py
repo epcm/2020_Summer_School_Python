7dbe042757b65ddc799f393dc33d7f88fd8193a5
epcm <zhangkaiyuan999@outlook.com>
2020-07-04 00:53:34 +0800

    refactor🎨 tree的参数提取

--- Day3/tree.py
+++ Day3/tree.py
@@ -4,6 +4,14 @@ from math import *
 import datetime
 random.seed(datetime.datetime.now())
 
+branch_len_control = (10, 15)
+angle_control = [30, 40]
+branch_shorten_rate = [80, 85]
+colors = ['#f44a55', 'white', '#ffcccc']
+#colors = ['black', 'white', 'black']
+#colors = ['#c71e2f', 'black', '#ffcccc']
+#colors = ['#896206', 'white', '#ffcccc']
+
 # 画正方形
 def square(size, color, t):
     t.fillcolor(color)
@@ -19,7 +27,7 @@ def leaf(size, color, t):
     t.pencolor(color)
     t.rt(90)
     t.backward(size/2)
-    #square(size, '#f44a55') #中
+    #square(size, colors[0]) #中
     t.rt(90)
     square(size, color, t) #左
     t.rt(180)
@@ -46,10 +54,10 @@ def leaf(size, color, t):
 def tree(branch_len, t):
     t.pensize(branch_len/3)
     t.fd(branch_len)
-    if branch_len > random.choice((10, 15)):
-        ang = random.randint(30,40)
+    if branch_len > random.choice(branch_len_control):
+        ang = random.randint(angle_control[0], angle_control[1])
         t.rt(ang)
-        per = random.randint(80,85)/100#枝长缩短因子    
+        per = random.randint(branch_shorten_rate[0], branch_shorten_rate[1])/100#枝长缩短因子    
         tree(branch_len*per, t)
         for _ in range(2):
             t.lt(ang)
@@ -60,10 +68,10 @@ def tree(branch_len, t):
         tow = t.heading()
         t1.seth(0)
         n = random.random()/10 + 0.9
-        uselessturtle = turtle.Turtle()
+        '''uselessturtle = turtle.Turtle()
         uselessturtle.pencolor(n, 0.8*n, 0.8*n)
-        LeafColor = uselessturtle.pencolor()
-        leaf(1.5, LeafColor, t)
+        LeafColor = uselessturtle.pencolor()'''
+        leaf(1.5, colors[2], t)
         t.seth(tow)
 
     t.penup()
@@ -80,10 +88,10 @@ def addition(n):
         t2.goto(x, -y)
         t1.pendown()
         t2.pendown()
-        t1.dot(6, '#f44a55')
-        t2.dot(6, 'white')
-        #leaf(2, '#f44a55', t1)
-        #leaf(2, 'white', t2)
+        t1.dot(6, colors[0])
+        t2.dot(6, colors[1])
+        #leaf(2, colors[0], t1)
+        #leaf(2, colors[1], t2)
 
 # 小恐龙
 ls = [
@@ -130,10 +138,11 @@ turtle.tracer(0)
 #ang = 30
 t1 = turtle.Turtle()
 t2 = turtle.Turtle()
-t1.pencolor('#f44a55')
+t1.pencolor(colors[0])
 
-# 下侧填充
-t1.fillcolor('#f44a55')
+# 绘制背景
+turtle.bgcolor(colors[1])
+t1.fillcolor(colors[0])
 t1.begin_fill()
 t1.fd(200)
 t1.rt(90)
@@ -146,7 +155,7 @@ t1.rt(90)
 t1.fd(200)
 t1.end_fill()
 
-t2.pencolor('white')
+t2.pencolor(colors[1])
 t1.left(90)
 t2.right(90)
 '''
@@ -168,11 +177,11 @@ t.fd(20)
 t.lt(90)
 t.fd(21*2)
 t.rt(90)
-t.pencolor('#f44a55')
-draw_by_str(ls, 2, '#f44a55', t, True)
-tt = turtle.Turtle()
+t.pencolor(colors[0])
+draw_by_str(ls, 2, colors[0], t, True)
 
-#tt.rt(180)
+# 镜像恐龙
+tt = turtle.Turtle()
 tt.penup()
 tt.fd(20)
 #tt.lt(90)
@@ -180,8 +189,21 @@ tt.rt(90)
 tt.fd(21*2)
 #tt.rt(90)
 tt.lt(90)
-tt.pencolor('white')
-draw_by_str(ls, 2, 'white', tt, False)
+tt.pencolor(colors[1])
+draw_by_str(ls, 2, colors[1], tt, False)
+
+'''
+#中心对称恐龙
+tt = turtle.Turtle()
+tt.rt(180)
+tt.penup()
+tt.fd(20)
+tt.lt(90)
+tt.fd(21*2)
+tt.rt(90)
+tt.pencolor(colors[1])
+draw_by_str(ls, 2, colors[1], tt, False)
+'''
 
 t.hideturtle()
 tt.hideturtle()
