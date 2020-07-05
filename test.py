758c01dd698d52cb17d10f9b89678c449a84e3cb
epcm <67184389+epcm@users.noreply.github.com>
2020-07-05 15:42:47 +0800

    test

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ for _ in range(10):
     temp = math.cos(math.radians(t.heading() + 5)) / 8 + 0.25
     t.pencolor(temp*1.6, temp*1.2, temp*1.4)
     t.fd(100)
-    print(temp)
 turtle.done()
-colors = ['#f44a55', 'white', '#ffcccc']
+
+#colors = ['#f44a55', 'white', '#ffcccc']
+print('assd')
