ea3ba4c619836c66887d28b6d1f86f554d5ed404
epcm <zhangkaiyuan999@outlook.com>
2020-07-05 15:44:51 +0800

    new

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-import turtle
-import math
-import random
-import datetime
+import pgzrun
 
-random.seed(datetime.datetime.now())
-t = turtle.Turtle()
+RED = 200, 0, 0
+BOX = Rect((100, 20), (200, 100))
 
-for _ in range(10):
-    t.setheading(random.randint(0, 360))
-    temp = math.cos(math.radians(t.heading() + 5)) / 8 + 0.25
-    t.pencolor(temp*1.6, temp*1.2, temp*1.4)
-    t.fd(100)
-    print(temp)
-turtle.done()
-colors = ['#f44a55', 'white', '#ffcccc']
+ls = []
+
+def draw():
+    screen.draw.rect(BOX, RED)
+
+pgzrun.go()
